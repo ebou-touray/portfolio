2ac3a1da568047ef18bc428ad556b05111298f20
test <eb2ray@gmail.com>
2021-08-29 18:51:11 +0300

    Added Projects, Technologies, About and personal accomplishments

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+<Section row nopadding>
+  <LeftSection>
+    <SectionTitle main center>
+      Hello there, <br />
+      I am Ebou, a Full Stack Web Developer
+    </SectionTitle>
+    <SectionText>
+      I will build web app using modern technologies:  React, Node, JavaScript ES6, Node, Mongo.
+    </SectionText>
+    <Button onClick={() => window.location = 'https://google.com'}>Learn more</Button>
+  </LeftSection>
+</Section>
 );
 
 export default Hero;
